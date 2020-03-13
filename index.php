502433061c92f86c7eb62577599cfa56ae64e007
oc admin <admin@oc.com>
2020-03-13 04:27:22 +0000

    okp commit

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page of oc</h1>
+<h1>My first PHP page by using ocp</h1>
 
 <?php
-echo "Hello World! Welcome to all";
+echo "Hello World! Welcome to all! ";
 ?>
 
 </body>
